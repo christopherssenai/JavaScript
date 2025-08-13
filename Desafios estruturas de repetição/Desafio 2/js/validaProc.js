c44ef8c5684d1261f41789762b6979c98e4f7a30
christopherssenai <christopher_steudel@estudante.sesisenai.org.br>
2025-08-13 19:56:14 -0300

    desenvolvendo desafio 2

--- Desafios estruturas de repetição/Desafio 2/js/validaProc.js
+++ Desafios estruturas de repetição/Desafio 2/js/validaProc.js
@@ -1,16 +1,6 @@
-function validaProc(){
-    if(document.frmnumero.txtnum.value==""){
-        alert("Preencha o campo Número");
-        document.frmnumero.txtnum.focus();
-        return false
-    }else{
-        let nu=parseInt(document.getElementById('num').value);
-        cont=1;
-        console.log("Tabuada do número "+nu);
-        do{
-            console.log(nu+" X "+cont+" = "+ nu * cont);
-            cont=cont+1;
-        }while(cont<=10)
-            return false;    
-    }
+function validaProc(){   
+    let peso=parseFloat(document.getElementById('pes').value)
+    let altura=parseFloat(document.getElementById('alt').value)
+    imc = peso/(altura * altura)
+    alert(imc)
 }
