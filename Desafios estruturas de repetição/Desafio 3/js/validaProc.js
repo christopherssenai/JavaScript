ffed948dcef8d766056c10cd1a4eaba3b2080532
christopherssenai <christopher_steudel@estudante.sesisenai.org.br>
2025-08-13 21:49:32 -0300

    finalizando desafio 1 2 e 3

--- Desafios estruturas de repetição/Desafio 3/js/validaProc.js
+++ Desafios estruturas de repetição/Desafio 3/js/validaProc.js
@@ -1,27 +1,42 @@
-function validaProc() {
-    let turma = 1;
-    let totalAlunosAcima7 = 0;
-    let somaMediasEscola = 0;
-    let totalAlunosEscola = 0;
+function validaProc(){
+        let idade;
+        let somaIdade = 0;
+        let totalPessoas = 0;
+        let genero;
+        let salarioMasculino = 0;
+        let salarioFeminino = 0;
+        let totalMasculino = 0;
+        let totalFeminino = 0;
+        let totalSalarios = 0;
+        let continuar;
+        
+    do{    
+        do{
+            idade = parseInt(prompt("Digite a idade: "))
+            if (idade < 1){
+                alert("Digite uma idade valida!");
+            }
+            somaIdade += idade;
 
-    do {
-        let media = parseFloat(prompt("Digite a média do único aluno da Turma " + turma + ":"));
+        }while(idade < 1)
+            genero = parseInt(prompt("Digite (M) para masculino e (F) para feminino: "));
 
-        somaMediasEscola += media;
-        totalAlunosEscola++;
+            if("M"){
+                totalPessoas++;
+                salarioMasculino = parseInt(prompt("Salario: "));
+                totalMasculino += salarioMasculino
+            }
+            else if ("F"){
+                totalPessoas++;
+                salarioFeminino = parseInt(prompt("Salario: "));
+                totalFeminino += salarioFeminino
+            }
+            continuar = prompt("deseja finalizar? "); 
+     }while(continuar!="FINALIZAR")   
+        totalSalarios = salarioFeminino + salarioMasculino
 
-        if (media >= 7) {
-            totalAlunosAcima7++;
-        }
-
-        console.log("Turma " + turma + ": média do aluno = " + media);
-
-        turma++;
-    } while (turma <= 3);
-
-    let mediaGeralEscola = somaMediasEscola / totalAlunosEscola;
-
-    console.log("Total de alunos com média >= 7: " + totalAlunosAcima7);
-    console.log("Média geral da escola: " + mediaGeralEscola.toFixed(2));
+        console.log("A média salarial é de: " +totalSalarios/totalPessoas);
+        console.log("A média de idade é: "+somaIdade/totalPessoas);
+        console.log("A média salarial feminina é de: " +totalFeminino/totalPessoas);
+        console.log("A média Salarial masculina é de: " +totalMasculino/totalPessoas);    
 }
-
